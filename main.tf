415627b732943f54b2578f8edc722b2461609fe6
skyroadz <shiko.ali@outlook.com>
2024-03-10 04:47:44 -0400

    add second node group

--- main.tf
+++ main.tf
@@ -12,4 +12,29 @@ module "eks" {
 
 }
 
+module "eks_managed_node_group" {
+  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
 
+  name            = "spot"
+  cluster_name    = module.eks.cluster_name
+  cluster_version = module.eks.cluster_version
+
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
+  vpc_security_group_ids            = [module.eks.node_security_group_id]
+
+
+  min_size     = 2
+  max_size     = 5
+  desired_size = 2
+
+  instance_types = ["t3.large"]
+  capacity_type  = "SPOT"
+
+  labels = {
+    role = "spot"
+
+  }
+
+}
