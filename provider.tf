e502320b903c95dd4807c124cf8792a8e6c66781
skyroadz <shiko.ali@outlook.com>
2024-03-11 00:27:29 -0400

    increase worker nodes

--- provider.tf
+++ provider.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.34"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
   default_tags {
